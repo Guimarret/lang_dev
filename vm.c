9e4962e2160e754347d1b8bbd34729b3d069cf51
Guilherme <gulherme.marret@g.globo>
2025-03-18 20:05:29 -0300

    feat: define native fun and added time lib

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -44,6 +45,14 @@ static void runtimeError(const char* format, ...) {
   int line = frame->function->chunk.lines[instruction];
   fprintf(stderr, "[line %d] in script\n", line);
   resetStack();
+}
+
+static void defineNative(const char* name, NativeFn function) {
+  push(OBJ_VAL(copyString(name, (int)strlen(name))));
+  push(OBJ_VAL(newNative(function)));
+  tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+  pop();
+  pop();
 }
 
 void initVM() {
