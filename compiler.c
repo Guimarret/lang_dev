39c6eb7ac626b687ce1988ba4e657071eceaf939
Guilherme <gulherme.marret@g.globo>
2025-03-18 19:43:29 -0300

    feat: added compiler struct new params for function support, including name attach and scope depth

--- compiler.c
+++ compiler.c
@@ -177,10 +177,23 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) {
-  compiler->localCount = 0;
-  compiler->scopeDepth = 0;
-  current = compiler;
+static void initCompiler(Compiler* compiler, FunctionType type) {
+    compiler->enclosing = current;
+    compiler->function = NULL;
+    compiler->type = type;
+    compiler->localCount = 0;
+    compiler->scopeDepth = 0;
+    compiler->function = newFunction();
+    current = compiler;
+    if (type != TYPE_SCRIPT) {
+      current->function->name = copyString(parser.previous.start,
+                                           parser.previous.length);
+    }
+
+    Local* local = &current->locals[current->localCount++];
+    local->depth = 0;
+    local->name.start = "";
+    local->name.length = 0;
 }
 
 static void endCompiler() {
