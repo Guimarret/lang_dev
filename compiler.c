d193c08565e98367b44f00ac74ddbc656e1b0fe8
Guilherme <gulherme.marret@g.globo>
2025-03-18 19:46:11 -0300

    feat: added return statement function that diffs if is a top level or not function

--- compiler.c
+++ compiler.c
@@ -315,6 +315,19 @@ static void printStatement() {
   emitByte(OP_PRINT);
 }
 
+static void returnStatement() {
+    if (current->type == TYPE_SCRIPT) {
+      error("Can't return from top-level code.");
+    }
+    if (match(TOKEN_SEMICOLON)) {
+        emitReturn();
+    } else {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+        emitByte(OP_RETURN);
+    }
+}
+
 static void whileStatement() {
   int loopStart = currentChunk()->count;
   consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
