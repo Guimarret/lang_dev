a406ca0c86fb03f00829ac580430c54be138d531
Guilherme <gulherme.marret@g.globo>
2025-03-18 20:05:10 -0300

    feat: call function with arity verifier and vm execution of the function (call) and call value set if is a normal function or native ones and makes a call if its a normal fun

--- vm.c
+++ vm.c
@@ -75,6 +75,43 @@ static Value peek(int distance) {
   return vm.stackTop[-1 - distance];
 }
 
+static bool call(ObjFunction* function, int argCount) {
+    if (argCount != function->arity) {
+        runtimeError("Expected %d arguments but got %d.",
+            function->arity, argCount);
+        return false;
+      }
+    if (vm.frameCount == FRAMES_MAX) {
+        runtimeError("Stack overflow.");
+        return false;
+    }
+    CallFrame* frame = &vm.frames[vm.frameCount++];
+    frame->function = function;
+    frame->ip = function->chunk.code;
+    frame->slots = vm.stackTop - argCount - 1;
+    return true;
+}
+
+static bool callValue(Value callee, int argCount) {
+  if (IS_OBJ(callee)) {
+    switch (OBJ_TYPE(callee)) {
+        case OBJ_FUNCTION:
+            return call(AS_FUNCTION(callee), argCount);
+        case OBJ_NATIVE: {
+          NativeFn native = AS_NATIVE(callee);
+          Value result = native(argCount, vm.stackTop - argCount);
+          vm.stackTop -= argCount + 1;
+          push(result);
+          return true;
+        }
+      default:
+        break; // Non-callable object type.
+    }
+  }
+  runtimeError("Can only call functions and classes.");
+  return false;
+}
+
 static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
