7038032b25d2565c13aafc1549a4af03a2376304
Guilherme <gulherme.marret@g.globo>
2025-03-20 09:12:44 -0300

    feat: closure and basically scope tracing amplified

--- object.h
+++ object.h
@@ -6,9 +6,11 @@
 #include "chunk.h"
 
 #define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+#define IS_CLOSURE(value)      isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value)       isObjType(value, OBJ_STRING)
+#define AS_CLOSURE(value)      ((ObjClosure*)AS_OBJ(value))
 #define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
 #define AS_NATIVE(value) \
     (((ObjNative*)AS_OBJ(value))->function)
@@ -17,18 +19,22 @@
 
 typedef enum {
   OBJ_STRING,
+  OBJ_UPVALUE,
+  OBJ_CLOSURE,
   OBJ_FUNCTION,
   OBJ_NATIVE,
 } ObjType;
 
 struct Obj {
   ObjType type;
+  bool isMarked;
   struct Obj* next;
 };
 
 typedef struct {
   Obj obj;
   int arity;
+  int upvalueCount;
   Chunk chunk;
   ObjString* name;
 } ObjFunction;
@@ -40,7 +46,6 @@ typedef struct {
   NativeFn function;
 } ObjNative;
 
-
 struct ObjString {
   Obj obj;
   int length;
@@ -48,10 +53,26 @@ struct ObjString {
   uint32_t hash;
 };
 
+typedef struct ObjUpvalue {
+  Obj obj;
+  Value* location;
+  Value closed;
+  struct ObjUpvalue* next;
+} ObjUpvalue;
+
+typedef struct {
+  Obj obj;
+  ObjFunction* function;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
+} ObjClosure;
+
+ObjClosure* newClosure(ObjFunction* function);
 ObjFunction* newFunction();
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
+ObjUpvalue* newUpvalue(Value* slot);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
